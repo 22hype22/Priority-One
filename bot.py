6e71da57ff610c0f7d618dcde8785f2d51e8deb4
22hype22 <everant00@gmail.com>
2026-04-15 22:11:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1059,7 +1059,7 @@ async def on_command_error(ctx, error):
 async def on_ready():
     await bot.change_presence(
         status=discord.Status.online,
-        activity=discord.Game(name="Keeping an eye on Oversites Community"),
+        activity=discord.Game(name="Keeping an eye on the Oversite community"),
     )
     bot.add_view(TicketActionView())
     await restore_ticket_panels(bot)
